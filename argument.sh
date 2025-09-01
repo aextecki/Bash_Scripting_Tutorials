4bc43761e8151ad35631daef03ee7e7f82ab9fc1
Alec <alec.aextecki@gmail.com>
2025-09-01 21:48:49 +0800

    Create argument.sh
    First Commit Arguments

---
+++ argument.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+# Command Arguments: $1, $2, etc., are special variables that hold the
+# values of the first, second, etc., command line arguments.
+
+echo "First argument: $1"
+echo "Second argument: $2"
+echo "Third argument: $3"
