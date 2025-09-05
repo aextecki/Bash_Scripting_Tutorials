9cc4864adb16bae1be9ecc423c30378078430dbf
aextecki <alec.aextecki@gmail,com>
2025-09-05 20:47:39 +0800

    Adding some Comment

--- forloop.sh
+++ forloop.sh
@@ -6,18 +6,8 @@ do
     echo " this is the $i"
 done
 
-<<<<<<< HEAD
 # using brace expansion 
 for t in {1..10}; do
     echo "This is the $t"
 done        
 
-=======
-#For loop:
-#The for loop is used when you want to repeat a block of code a known number of times. Instead of
-#checking a condition, it iterates over a sequence of values.
-#Analogy:
-#Think of the `for loop` as an car assembly line. You have to put some parts. 
-#take it one car at a time, test it, and then move on to the next. You keep doing this until you've
-#finish all the car in a day.
->>>>>>> c200b9c80f9b4a76dbf86915c7a792bba85833c4
