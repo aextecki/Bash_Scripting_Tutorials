1ec25509752b483489f3747c1c758555a90ee1e2
aextecki <alec.aextecki@gmail,com>
2025-09-05 20:36:36 +0800

    adding with brace expansion Example

--- forloop.sh
+++ forloop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
-
+#using for loop to print numbers from 1 to 10
 for i in 1 2 3 4 5 6 7 8 9 0 
 
 do 
     echo " this is the $i"
 done
+
+# using brace expansion 
+for t in {1..10}; do
+    echo "This is the $t"
+done        
+
