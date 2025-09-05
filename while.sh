3ed6f583ca66e6f40c33dfcc16eb0e2f73bb08e0
ModRockxy <alec.aextecki@gmail.com>
2025-09-06 01:21:13 +0800

    Create while.sh
    creation of using while

---
+++ while.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+#A while loop repeatedly runs a block of code as long as a specific condition remains true.
+#Analogy: Imagine a while loop like brewing coffee each morning. You keep pouring water into the coffee maker (looping) as long as there are coffee grounds in the filter #(condition is true). Once the grounds are used up or the pot is full (condition is false), you stop brewing.
+
+#sample 
+#whie [condition]; do
+    # command to run 
+#done
+
+#Condition: This is the check that decides whether the loop continues or stops. 
+#If the condition evaluates to true, the loop keeps running; if it’s false, the loop terminates.
+
+#Loop Body: This is the set of instructions that runs repeatedly as long as the condition remains true.
+
